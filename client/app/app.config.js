653787dbca6a8243e4afb2333708e75cc01ff6fc
infinityminus1 <kurtalang@gmail.com>
2016-01-27 18:59:45 -0800

    Change base url

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -3,6 +3,6 @@
 angular.module("robobetty")
     .constant("appConfig", {
         isMobile: IS_MOBILE || false, 
-        baseUrl: "https://blue-jay.herokuapp.com/",
+        baseUrl: "https://spotonsoftware.herokuapp.com/",
         debugMode: false
     });
