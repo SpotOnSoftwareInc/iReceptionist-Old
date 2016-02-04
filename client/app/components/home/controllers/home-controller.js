bea6d328a51d4d2395a97524a2c8207f6836db30
Kurt Lang <kurtalang@gmail.com>
2016-02-04 11:17:00 -0800

    Working on home page and cloaked signin

---
+++ client/app/components/home/controllers/home-controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('home')
+    .controller('HomeController', ['$scope',
+        function ($scope) {
+            //function that sets the background by using the getTheme method of the CheckinService
+            //also, uses the CheckinService to get the forms that the business wants to display
+            $scope.init = function() {
+                CheckinService.getTheme($rootScope.admin_id)
+                    .success(function (data) {
+                        if (data == "null" || data.background_img == "default") {
+                            $scope.background_image = "images/themes/city0.jpg";
+                        }
+                        else {
+                            $scope.background_image = data.background_img;
+                        }
+                        return data;
+                    })
+                    .error(function (err) {
+                        return err;
+                    });
+            }
+
+        }]);
